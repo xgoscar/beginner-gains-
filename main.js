ca76b438f10673642d07c6e37232964971dd2efb
Oscar Garcia <goscar1996@gmail.com>
2018-10-24 18:54:31 -0700

    ...
    ..

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log('Hello!')
+$.getJSON('https://api.airtable.com/v0/app57qa27H9exSnqq/Chest?api_key=keyuEvAoHG6yJhC2T',
+  function(airtable){
+    var html = [];
+    $.each(airtable.records, function(index, record) {
+      var name = record.fields['Name'];
+      var address = record.fields['Address'];
+      var rating = record.fields['Rating'];
+      html.push(`<h2>${name}, ${address}, ${rating}</h2>`);
+    });
+    $('body').append(html);
+  }
+);
