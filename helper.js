3d0aa8d1c56257645de470e0898cb3e25300f134
Oscar Garcia <goscar1996@gmail.com>
2018-12-06 14:43:47 -0800

    fixed most bugs

---
+++ helper.js
@@ -0,0 +1,12 @@
+// Helper for getting the `index.html?ID=` part form the URL
+var getParameterByName = function(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"), results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
+  
+  // Airtable API Key, unique per user
+  var api_key = 'keyuEvAoHG6yJhC2T';
--- main.js
+++ main.js
@@ -1,10 +1,76 @@
-$.getJSON('https://api.airtable.com/v0/app57qa27H9exSnqq/Chest?api_key=keyuEvAoHG6yJhC2T',
-  function(airtable){
-    var html = [];
-    $.each(airtable.records, function(index, record) {
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+ var getAllRecords = function() {
+  $.getJSON('https://api.airtable.com/v0/app57qa27H9exSnqq/Chest?api_key=keyuEvAoHG6yJhC2T',
+    function(airtable){
+      var html = [];
+      $.each(airtable.records, function(index, record) {
+        var id = record.id;
+        var name = record.fields['Name'];
+        var sets = record.fields['Sets'];
+        var picture = record.fields['Pictures'];
+        var rest = record.fields['Rest'];
+        var type = record.fields['Type'];
+        var part = record.fields['Part'];
+        html.push(listView(id, name, sets, picture, rest, type, part));
+      });
+      $('body').append(html);
+    }
+  );
+}
+ var getOneRecord = function(id) {
+  $.getJSON('https://api.airtable.com/v0/app57qa27H9exSnqq/Chest/${id}?api_key=${api_key}',
+    function(record){
+      var html = [];
       var name = record.fields['Name'];
-      html.push(`<h2>${name}</h2>`);
-    });
-    $('body').append(html);
-  }
-);
+      var address = record.fields['Address'];
+      var rating = record.fields['Rating'];
+      var picture = record.fields['Pictures'];
+      var cost = record.fields['Cost'];
+      var type = record.fields['Type'];
+      html.push(detailView(name, address, rating, picture, cost, type ));
+      $('body').append(html);
+    }
+  );
+}
+ var listView = function(id, name, sets, picture, rest, type, part) {
+  return `
+  <div class="card text-center" style="width: 18rem; display: inline-flex;">
+    <div class="card-body ">
+    ${picture ? `<img src="${picture[0].url}">` : ``}
+      <h2>${name}</h2>
+      <p class="card-text">${sets}</p>
+      <p class="card-text">${part}</p>
+      <p class="card-text">${rest} of rest between sets</p>
+      <a href="workout.html?id=${id}" class="btn bg-dark">View</a>
+      <div class="d-flex justify-content-between align-items-center">
+      </a>
+    </div>
+  </div>
+</div>
+   
+  `;
+}
+ var detailView = function(name, address, rating, picture, cost, type) {
+  return `
+    <h2>${name}</h2>
+    <p>${address}</p>
+    <p>${rating}</p>
+    <p>${cost}</p>
+    <p>${type}</p>
+    ${picture ? `<img src="${picture[0].url}">` : ``}
+  `;
+}
+ var id = getParameterByName('id');
+if (id) {
+  getOneRecord(id);
+} else {
+  getAllRecords();
+}
